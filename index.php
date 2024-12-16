189d561b75e49763c85802ad3cc62b82c0466641
Filip <filip1kop@gmail.com>
2024-12-16 09:24:23 +0100

    need to do more css

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include('phpqrcode/qrlib.php');
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>QR Code Generator</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
      <div class="container">
